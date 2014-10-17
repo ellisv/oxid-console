7d296bad67107bf9b603e4e5e2103736af4ba5fe
Robin Lehrmann <robin@snow-hamster.de>
2014-10-17 23:10:46 +0200

    implement composer

---
+++ metadata.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robin
+ * Date: 17.10.14
+ * Time: 21:29
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.0';
+
+/**
+ * Module information
+ */
+$aModule = array(
+    'id'          => 'oxid-console',
+    'title'       => 'console',
+    'description' => 'OXID Console is php console application for OXID Shop. It provides an API for writting various commands.',
+    'thumbnail'   => 'picture.png',
+    'version'     => '1.0',
+    'author'      => 'Eligijus Vitkauskas',
+    'extend'      => array(
+    ),
+    'files'       => array(
+    ),
+    'blocks'      => array(
+    )
+);
