8eb22ff0e32dc2e6c161565a84368867c282399b
Christian Loock <chl@vkf-renzel.de>
2014-10-29 16:03:45 +0100

    Fixed licence header to say Oxid Console again

--- copy_this/core/oxmigrationhandler.php
+++ copy_this/core/oxmigrationhandler.php
@@ -13,7 +13,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OXID eShop Community Edition.  If not, see <http://www.gnu.org/licenses/>.
+ * along with OXID Console.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author        OXID Professional services
  * @link          http://www.oxid-esales.com
