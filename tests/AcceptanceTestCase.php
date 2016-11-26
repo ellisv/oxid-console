d0c4def0ce52cf32dc9646187bacdbfa53feaa08
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2016-11-26 21:00:20 +0200

    Adjust pathes for AcceptanceTestCase to reflect new directory stucture that prepare-oxid is making

--- tests/AcceptanceTestCase.php
+++ tests/AcceptanceTestCase.php
@@ -5,13 +5,13 @@ class AcceptanceTestCase extends PHPUnit_Framework_TestCase
 
     protected static function runConsole($line = '')
     {
-        chdir(__DIR__ . '/../oxid');
+        chdir(__DIR__ . '/../oxid/source');
         return shell_exec(static::getExecutablePath() . ' ' . $line);
     }
 
     protected static function getExecutablePath()
     {
-        return __DIR__ . '/../bin/oxid';
+        return __DIR__ . '/../../bin/oxid';
     }
 
     protected static function removeDirectory($dir)
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../oxid/bootstrap.php';
+require_once __DIR__ . '/../oxid/source/bootstrap.php';
 
 require_once __DIR__ . '/AcceptanceTestCase.php';
