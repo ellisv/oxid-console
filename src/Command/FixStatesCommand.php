7dbc207f5ef844f5b49add4cb48a77e3a4e2e576
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2016-12-05 20:47:47 +0200

    Add missing fileheaders for commands

--- src/Command/FixStatesCommand.php
+++ src/Command/FixStatesCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OXID Console package.
+ *
+ * (c) Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Ellis\Oxid\Console\Command;
 
 use Ellis\Oxid\Console\Util\ModuleUtil;
